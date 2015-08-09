2fe5b150e0d594cea8e1d8aa3f307b3cabb7a589
Rob Spectre <rob.spectre@gmail.com>
2015-08-08 20:24:05 -0400

    1) Added django startup to gulp. 2) Added Google Analytics tracking code.

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,27 @@ var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var livereload = require('gulp-livereload');
 
+var exec;
+exec = require('child_process').exec;
+
 /* Watch Files For Changes */
 gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('**/static/js/*.js').on('change', livereload.changed);
-    gulp.watch('**/static/css/*.css').on('change', livereload.changed);
-    gulp.watch('**/templates/**').on('change', livereload.changed);
+  livereload.listen();
+  gulp.watch('**/static/js/*.js').on('change', livereload.changed);
+  gulp.watch('**/static/css/*.css').on('change', livereload.changed);
+  gulp.watch('**/templates/**').on('change', livereload.changed);
 
 });
 
-gulp.task('default', ['watch']);
+gulp.task('django', function() {
+  var proc;
+  proc = exec('python adcap_website/manage.py runserver');
+  proc.stderr.on('data', function(data) {
+    return process.stdout.write(data);
+  });
+  return proc.stdout.on('data', function(data) {
+    return process.stdout.write(data);
+  });
+});
+
+gulp.task('default', ['django', 'watch']);
