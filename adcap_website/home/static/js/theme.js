256c2fc0940078675107681934e77623a98c723c
Rob Spectre <rob@brooklynhacker.com>
2016-03-19 13:26:22 -0400

    1) Tweaks to StatsBlock. 2) Trying to fix goddamn parallax scrolling.

--- adcap_website/home/static/js/theme.js
+++ adcap_website/home/static/js/theme.js
@@ -593,7 +593,7 @@ function shortcodeElements() {
     //Parallax plugin Js Function element
     $.stellar({
         horizontalScrolling: false,
-        verticalOffset: 0
+        verticalOffset: 150
     });
 
 
